aada7743419eefb3f7f9b46a4f5ab9bf61a4698b
Libin Biji <libinbiji43@gmail.com>
2023-04-18 12:33:28 +0530

    faculty added

--- src/components/schoolAdmin/AddClassRoom.jsx
+++ src/components/schoolAdmin/AddClassRoom.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import { useNavigate, Link } from 'react-router-dom'
+import { useGetFacultyDropDownQuery } from '../../api/schoolAdmin/apiSlice';
 
 const AddClassRoom = () => {
-    const { 
+    const {data, isLoading, isError} = useGetFacultyDropDownQuery();
+    if(data){
+        console.log('got ', data);
+    }
+    const {
         register,handleSubmit, watch, formState: { errors } 
     } = useForm();
     
-    const onSubmit =  (data) => {   
+    const onSubmit = async (data) => {
         console.log('on sugmit called in studet');
         console.log(data);
     }
   return (
     <div className='addClassRoom'>
+        <div className="m-3 ml-20 absolute inset-0 text-xl text-gray-900 font-semibold">
           <div className="p-4 sm:ml-64">
               <div className="p-4 border-2 border-gray-200 border-none rounded-lg dark:border-gray-700 mt-14">
                   <div className='p-4 border-2 border-gray-200 border-none rounded-lg dark:border-gray-700 mt-14'>
@@ -29,9 +35,19 @@ const AddClassRoom = () => {
               />
 
               </svg>
-                  <form onSubmit={handleSubmit(onSubmit)}>
+              {
+                isLoading?
+                <div role="status" className='flex justify-center items-center h-screen'>
+                <svg aria-hidden="true" className="w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                </svg>
+                <span className="sr-only">Loading...</span>
+            </div>
+            :
+            <form onSubmit={handleSubmit(onSubmit)}>
                     <h5>Add Classrooms</h5> 
-                      <div className="grid gap-6 mb-6 md:grid-cols-2">
+                      <div className="grid gap-6 mb-6 md:grid-cols-3">
                           <div>
                               <label
                                   htmlFor="class_name"
@@ -46,7 +62,7 @@ const AddClassRoom = () => {
                                   {...register('className',{
                                     required: 'Class is required',
                                     pattern:{
-                                        value:/(^\d{1,2}$)/,
+                                        value:/(^[0-9]{1,2} [A-Z]$)/,
                                         message:'Enter a valid class'
                                     },
                                 })}
@@ -57,61 +73,6 @@ const AddClassRoom = () => {
                                </p>
                           </div>
                           
-                          <div>
-                              <label
-                                  htmlFor="section_name"
-                                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                              >
-                                  Section
-                              </label>
-                              <input
-                                  type="text"
-                                  id="section_name"
-                                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-stone-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  {...register('sectionName',{
-                                    required: 'Section is required',
-                                    pattern:{
-                                        value:/(^[A-Z]+$)/,
-                                        message:'Enter a valid section name'
-                                    }
-                    
-                                })}
-                                  placeholder="Enter section name  [A-Z]"
-                              />
-                              <p className="text-xs italic text-red-500">
-                                 {errors.sectionName?.message}
-                               </p>
-                          </div>
-                          <div>
-                              <label
-                                  htmlFor="faculty_incharge"
-                                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                              >
-                                  Faculty Incharge
-                              </label>
-                              <input
-                                  type="text"
-                                  id="faculty"
-                                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-stone-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  {...register('facultyIncharge',{
-                                    required: 'Faculty is required',
-                                    pattern:{
-                                        value:/(^[+]?[0-9]{1,3}?[-\s.]?[0-9]{3,4}[-\s.]?[0-9]{4}$)/,
-                                        message:'Enter a valid phone number'
-                                    },
-                                    minLength:{
-                                        value: 10,
-                                        message:'Enter 10 digits'
-                                    },
-                                    maxLength:{
-                                        value: 10,
-                                        message:'phone number exceeds 10 digits'
-                                    }
-                    
-                                })}
-                                  placeholder="Enter mobile number"
-                              />
-                          </div>
                           <div>
                               <label
                                   htmlFor="academic_year"
@@ -126,25 +87,66 @@ const AddClassRoom = () => {
                                   {...register('academicYear',{
                                     required: 'Academic year is required',
                                     pattern:{
-                                        value:/(^[+]?[0-9]{1,3}?[-\s.]?[0-9]{3,4}[-\s.]?[0-9]{4}$)/,
+                                        value:/(^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/((19|20)\d{2})$)/,
                                         message:'Enter a valid date'
                                     },
                                 })}
                                   placeholder="dd/mm/yy"
                               />
+                               <p className="text-xs italic text-red-500">
+                                 {errors.academicYear?.message}
+                               </p>
                           </div>
+
+                                      <div> 
+                                          <label
+                                              htmlFor="faculty_incharge"
+                                              className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                                          >
+                                              Faculty Incharge
+                                          </label>
+                                          <select
+                                              id="faculty_incharge"
+                                              defaultValue=""
+                                              className="text-white bg-blue-700 dark:hover:bg-gray-600 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-4 py-2.5  inline-flex items-center dark:bg-stone-800 dark:border-gray-600 dark:focus:ring-blue-500"
+                                              {...register('facultyIncharge',{
+                                                required: true, 
+                                              })}
+                                          >
+                                              <option value="" disabled>Select your faculty</option>
+                                              {
+                                                data.facultyName.map((faculty,index) => {
+                                                    return(
+                                                        <option key={index} value={faculty._id}>
+                                                          {faculty.facultyName}
+                                                        </option>
+                                                    )
+                                                   
+                                                })
+                                              }
+                                          </select>
+                                          {errors.facultyIncharge && (
+                                              <p className="text-xs italic text-red-500">
+                                                  Please select a faculty incharge.
+                                              </p>
+                                          )}
+                                      </div>
+
                       </div>
                       <div className='flex flex-col items-center'>
                       <button
                           type="submit"
+                          
                           className="mx-auto mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:max-w-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                       >
                           Submit
                       </button>
                       </div>
                   </form>
+              }
                   </div>
               </div>
+          </div>
           </div>
       </div>
   )
--- src/components/schoolAdmin/AddFaculty.jsx
+++ src/components/schoolAdmin/AddFaculty.jsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate, Link } from 'react-router-dom'
+import { useAddFacultyMutation } from '../../api/schoolAdmin/apiSlice';
 
 const AddFaculty = () => {
+    const [isEmailTakenError,setIsEmailTakenError] = useState(false);
+    const [isPhoneTakenError,setIsPhoneTakenError] = useState(false);
+    const [addFaculty, {isLoading, isError}] = useAddFacultyMutation();
+
     const { 
         register,handleSubmit, watch, formState: { errors } 
     } = useForm();
 
-    const onSubmit =  (data) => {
+    const onSubmit = async (data) => {
         console.log('on sugmit called in faculty');
         console.log(data);
+        try{
+            const res = await addFaculty(data).unwrap()
+            console.log(res);
+        }
+        catch (error) {
+            console.log(error);
+            if(error.status === 409){
+                if(error.data.error === 'Email Already Exist'){
+                  setIsEmailTakenError(true)
+                  setTimeout(() => {
+                    setIsEmailTakenError(false);
+                  }, 3000);
+                }
+                else if(error.data.error === 'Phone Already Exist'){
+                  setIsPhoneTakenError(true)
+                  setTimeout(() => {
+                    setIsPhoneTakenError(false);
+                  }, 3000);
+                }
+              }
+        }
     }
   return (
       <div className='addFaculty'>
-          <div className="p-4 sm:ml-64">
-              <div className="p-4 border-2 border-gray-200 border-none rounded-lg dark:border-gray-700 mt-14">
+        <div className="m-3 ml-20 absolute inset-0 text-xl text-gray-900 font-semibold">
+
+          <div className="p-4 ">
+            {
+                isLoading ?
+                      <div role="status" className='flex justify-center items-center h-screen'>
+                          <svg aria-hidden="true" className="w-16 h-16 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                          </svg>
+                          <span className="sr-only">Loading...</span>
+                      </div>
+                      :
+                      <div className="p-4 border-2 border-gray-200 border-none rounded-lg dark:border-gray-700 mt-14">
                   <form onSubmit={handleSubmit(onSubmit)}>
                     <h5>Add faculty</h5>
                       <div className="grid gap-6 mb-6 md:grid-cols-2">
@@ -47,21 +85,7 @@ const AddFaculty = () => {
                                  {errors.facultyName?.message}
                                </p>
                           </div>
-                          {/* <div>
-                              <label
-                                  htmlFor="last_name"
-                                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                              >
-                                  Last name
-                              </label>
-                              <input
-                                  type="text"
-                                  id="last_name"
-                                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  placeholder="Doe"
-                                  required=""
-                              />
-                          </div> */}
+                          
                           <div>
                               <label
                                   htmlFor="email"
@@ -83,6 +107,14 @@ const AddFaculty = () => {
                                 })}
                                   placeholder="Enter email"
                               />
+                                      {
+                                          isEmailTakenError ?
+                                              <p className="text-xs italic text-red-500">
+                                                  Email already exists
+                                              </p>
+                                              :
+                                              null
+                                      }
                                <p className="text-xs italic text-red-500">
                                  {errors.email?.message}
                                </p>
@@ -116,6 +148,14 @@ const AddFaculty = () => {
                                 })}
                                   placeholder="Enter mobile number"
                               />
+                                      {
+                                          isPhoneTakenError ?
+                                              <p className="text-xs italic text-red-500">
+                                                  Phone number already exists
+                                              </p>
+                                              :
+                                              null
+                                      }
                               <p className="text-xs italic text-red-500">
                                  {errors.phone?.message}
                                </p>
@@ -129,9 +169,9 @@ const AddFaculty = () => {
                               </label>
                               <input
                                   type="text"
-                                  id="dob"
+                                  id="dateOfBirth"
                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-stone-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  {...register('dob',{
+                                  {...register('dateOfBirth',{
                                     required: 'Date of birth is required',
                                     pattern:{
                                         value:/(^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/((19|20)\d{2})$)/,
@@ -141,7 +181,7 @@ const AddFaculty = () => {
                                   placeholder="dd/mm/yy"
                               />
                               <p className="text-xs italic text-red-500">
-                                 {errors.dob?.message}
+                                 {errors.dateOfBirth?.message}
                                </p>
                           </div>
                           <div>
@@ -180,6 +220,8 @@ const AddFaculty = () => {
                   </form>
 
               </div>
+            }
+          </div>
           </div>
       </div>
   )
